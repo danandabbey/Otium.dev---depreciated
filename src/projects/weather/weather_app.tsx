f588b2c01d6885bc8e00dd71845be03ac97d9d0f
danandabbey <danandabbey502@gmail.com>
2023-10-26 14:46:12 -0400

    commit

--- src/projects/weather/weather_app.tsx
+++ src/projects/weather/weather_app.tsx
@@ -19,7 +19,7 @@ function weather_app() {
   const [data, setData]: any = useState<any>({});
   const [isLoading, setLoading]: any = useState(true);
   const ip = "34.173.169.155";
-  const port: number = 5443;
+  const port: number = 443;
   const serverURL = `https://${ip}:${port}`;
 
   const location: any = useContext(locationContext);
